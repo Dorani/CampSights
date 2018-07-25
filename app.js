7e8c5398c2354d82b0e6b600c61e1a8e9ae5577b
Seif <seif.dorani@gmail.com>
2018-07-24 18:18:48 -0700

    required express and initialized app to lsn for request to start the server on local host 3000

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+
+
+
+
+
+
+
+
+
+
+//tell express to listen for request (start server)
+app.listen(3000, function(){
+  console.log("CampSights server has started on 3000");
+});
