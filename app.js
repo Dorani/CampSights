489488ac7868b7e4d9f3dcaa94e32de71aefee8b
Seif <seif.dorani@gmail.com>
2018-07-24 18:46:30 -0700

    have a route for campgrunds that can be accessed from the landing page route via a link, hard coded an array of objects with images and names

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ app.get("/", function(req,res){
 });
 
 app.get("/campgrounds", function(req,res){
-  
+  var campgrounds = [
+    {name: "Salmon Creek", image:"https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104496f2c57da1ebb4bf_340.jpg"},
+    {name: "Granite Hill", image:"https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104496f2c57da1ebb4bf_340.jpg"},
+    {name: "Mountain Goat's Rest", image:"https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104496f2c57da1ebb4bf_340.jpg"}
+  ]
+
+  res.render("campgrounds");
 });
 
 
