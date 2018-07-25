352c27e9bea3f39e060cd6bd584e8efadd6dfef0
Seif <seif.dorani@gmail.com>
2018-07-24 18:25:50 -0700

    app set to view engine ejs types and now we will render to the root what we have in our landing ejs file

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
 
 app.get("/", function(req,res){
-  res.send("this will be the landing page soon");
+  res.render("landing");
 });
 
 
