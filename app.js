b71521e36cf16a705605c1265cd971fe60c6d093
Seif <seif.dorani@gmail.com>
2018-08-21 11:25:36 -0700

    works perfectly, reflecting 3 db items

--- app.js
+++ app.js
@@ -29,36 +29,19 @@ var campgroundSchema = new mongoose.Schema({
 //Compile into a model
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-Campground.create(
-  {
-      name: "Mountain Goat's Rest",
-      image:"https://cdn.pixabay.com/photo/2014/06/04/16/36/car-repair-362150_960_720.jpg"
-      
-  },  function(err, campground){
-    if (err){
-      console.log(err);
-    } else {
-      console.log("Newly creaetd campground : ");
-      console.log(campground);
-    }
-  });
+// Campground.create(
+//   {
+//       name: "Mountain Goat's Rest",
+//       image:"https://cdn.pixabay.com/photo/2014/06/04/16/36/car-repair-362150_960_720.jpg"
+//   },  function(err, campground){
+//     if (err){
+//       console.log(err);
+//     } else {
+//       console.log("Newly creaetd campground : ");
+//       console.log(campground);
+//     }
+//   });
 
-
-
-
-
-var campgrounds =
-[
-  {name: "Salmon Creek", image:"https://cdn.pixabay.com/photo/2017/09/26/13/50/rv-2788677_960_720.jpg"},
-  {name: "Granite Hill", image:"https://cdn.pixabay.com/photo/2014/05/03/00/42/vw-camper-336606_960_720.jpg"},
-  {name: "Mountain Goat's Rest", image:"https://cdn.pixabay.com/photo/2014/06/04/16/36/car-repair-362150_960_720.jpg"},
-  {name: "Salmon Creek", image:"https://cdn.pixabay.com/photo/2015/07/09/01/59/picnic-table-837221__340.jpg"},
-  {name: "Granite Hill", image:"https://cdn.pixabay.com/photo/2016/04/28/15/49/airstream-1359135__340.jpg"},
-  {name: "Mountain Goat's Rest", image:"https://cdn.pixabay.com/photo/2016/08/14/01/53/campground-1591949__340.jpg"},
-  {name: "Salmon Creek", image:"https://cdn.pixabay.com/photo/2017/10/07/01/01/bach-leek-2825197__340.jpg"},
-  {name: "Granite Hill", image:"https://cdn.pixabay.com/photo/2018/05/16/15/49/camper-3406137__340.jpg"},
-  {name: "Mountain Goat's Rest", image:"https://cdn.pixabay.com/photo/2017/07/17/16/21/nature-2512944__340.jpg"}
-];
 
 //root route with landing page
 app.get("/", function(req,res){
@@ -67,7 +50,14 @@ app.get("/", function(req,res){
 
 //campgrounds route with ejs file connected to render the obj
 app.get("/campgrounds", function(req,res){
-  res.render("campgrounds", {campgrounds: campgrounds});
+  //get all camgrounds from DB
+  Campground.find({}, function(err, allCamgrounds){
+    if (err){
+      console.log(err);
+    } else {
+      res.render("campgrounds", {campgrounds: allCamgrounds});
+    }
+  });
 });
 
 
