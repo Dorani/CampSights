4d5dc840057637a9c42b4484addf770387fc3976
Seif <seif.dorani@gmail.com>
2018-08-26 20:31:56 -0700

    edits

---
+++ methods.js
@@ -0,0 +1,14 @@
+//----------------Place for notes---------------------//
+
+// Campground.create(
+//   {
+//       name: "Mountain Goat's Rest",
+//       image:"https://cdn.pixabay.com/photo/2014/06/04/16/36/car-repair-362150_960_720.jpg"
+//   },  function(err, campground){
+//     if (err){
+//       console.log(err);
+//     } else {
+//       console.log("Newly creaetd campground : ");
+//       console.log(campground);
+//     }
+//   });
